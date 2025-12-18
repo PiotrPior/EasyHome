90f4456c5409f2ffb291d0b2aaaea62ded490831
tinilinsk <tema.ryabkov@gmail.com>
2025-12-18 11:24:51 +0100

    Added model product

--- EasyHome/Models/Products.cs
+++ EasyHome/Models/Products.cs
@@ -6,5 +6,11 @@ namespace EasyHome.Models
 {
     public class Products
     {
+        [SQLite.PrimaryKey, SQLite.AutoIncrement]
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public bool InStock { get; set; }
     }
 }
